38ef861c273282d575c22b43108931a75c3ce1ff
Larisa <larisa.plesescu05@e-uvt.ro>
2025-10-27 10:03:32 +0200

    lAB3

---
+++ src/main/java/com/example/sp_lab31/MySpringApplication.java
@@ -0,0 +1,32 @@
+package ro.uvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ro.uvt.difexamples.ClientComponent;
+import ro.uvt.difexamples.SingletonComponent;
+import ro.uvt.difexamples.TransientComponent;
+
+@SpringBootApplication
+public class MySpringApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MySpringApplication.class, args);
+
+        System.out.println("\n--- Transient Beans ---");
+        TransientComponent t1 = context.getBean(TransientComponent.class);
+        t1.operation();
+        TransientComponent t2 = context.getBean(TransientComponent.class);
+        t2.operation();
+
+        System.out.println("\n--- Singleton Beans ---");
+        SingletonComponent s1 = context.getBean(SingletonComponent.class);
+        s1.operation();
+        SingletonComponent s2 = context.getBean(SingletonComponent.class);
+        s2.operation();
+
+        System.out.println("\n--- Client Component ---");
+        ClientComponent c = context.getBean(ClientComponent.class);
+        c.operation();
+    }
+}
