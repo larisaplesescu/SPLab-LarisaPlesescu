97bedd403f8e450b5fd7cd44353c49840dc5a3b0
Larisa <larisa.plesescu05@e-uvt.ro>
2025-10-27 10:06:17 +0200

    lAB3

---
+++ src/main/java/com/example/sp_lab31/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package ro.uvt.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ro.uvt.difexamples.ClientComponent;
+
+@RestController
+public class HelloController {
+
+    private final ClientComponent clientComponent;
+
+    public HelloController(ClientComponent clientComponent) {
+        this.clientComponent = clientComponent;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from " + clientComponent;
+    }
+}
